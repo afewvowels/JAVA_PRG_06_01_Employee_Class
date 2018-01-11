277a18932740684886b35d12de5d74b62afde55d
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-11 14:10:10 -0500

    Initial project and classes created.

--- src/java_prg_06_01_employee_class/Employee.java
+++ src/java_prg_06_01_employee_class/Employee.java
@@ -10,5 +10,24 @@ package java_prg_06_01_employee_class;
  * @author bluebackdev
  */
 public class Employee {
+    private String strName;
+    private String strDepartment;
+    private String strPosition;
     
+    private int intIDNumber;
+    
+    /**
+        Constructor
+        @param name The employee's name.
+        @param idNum The employee's ID Number.
+        @param dept The employee's department.
+        @param pos The employee's position.
+     */
+    
+    public Employee(String name, int idNum, String dept, String pos) {
+        strName = name;
+        intIDNumber = idNum;
+        strDepartment = dept;
+        strPosition = pos;
+    }
 }
