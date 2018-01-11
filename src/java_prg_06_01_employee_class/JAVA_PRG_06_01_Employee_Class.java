13a44f18644e29d9f919867f6dd72c96c6582949
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-11 14:23:16 -0500

    Initial version tested and working.

--- src/java_prg_06_01_employee_class/JAVA_PRG_06_01_Employee_Class.java
+++ src/java_prg_06_01_employee_class/JAVA_PRG_06_01_Employee_Class.java
@@ -15,7 +15,42 @@ public class JAVA_PRG_06_01_Employee_Class {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Employee Susan = new Employee();
+        Employee Mark = new Employee();
+        Employee Joy = new Employee();
+        
+        Susan.setName("Susan Meyers");
+        Susan.setIDNum(47899);
+        Susan.setDepartment("Accounting");
+        Susan.setPosition("Vice President");
+        
+        Mark.setName("Mark Jones");
+        Mark.setIDNum(39119);
+        Mark.setDepartment("IT");
+        Mark.setPosition("Programmer");
+        
+        Joy.setName("Joy Rogers");
+        Joy.setIDNum(81774);
+        Joy.setDepartment("Manufacturing");
+        Joy.setPosition("Engineer");
+        
+        System.out.println(Susan.getName());
+        System.out.println(Susan.getIDNum());
+        System.out.println(Susan.getDepartment());
+        System.out.println(Susan.getPosition());
+        System.out.println("\n");
+        
+        System.out.println(Mark.getName());
+        System.out.println(Mark.getIDNum());
+        System.out.println(Mark.getDepartment());
+        System.out.println(Mark.getPosition());
+        System.out.println("\n");
+        
+        System.out.println(Joy.getName());
+        System.out.println(Joy.getIDNum());
+        System.out.println(Joy.getDepartment());
+        System.out.println(Joy.getPosition());
+        System.out.println("\n");
     }
     
 }
