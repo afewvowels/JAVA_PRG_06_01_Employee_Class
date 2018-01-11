ab4908c898a75342dbd418ef554757645db41eb2
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-11 14:03:39 -0500

    Initial project and classes created.

---
+++ src/java_prg_06_01_employee_class/Employee.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_06_01_employee_class;
+
+/**
+ *
+ * @author bluebackdev
+ */
+public class Employee {
+    
+}
